768438b468522761fee75f8b693b5d9df931c47e
ManiCloudGuru <43004552+ManiCloudGuru@users.noreply.github.com>
2018-12-31 11:04:17 -0800

    Add files via upload

---
+++ Id4Server.ConsoleClientApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace Id4Server.ConsoleClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Client Credentials
+            /*var tokenClient = new TokenClient("http://localhost:5000/connect/token", "client", "secret");
+            var tokenResponse =  tokenClient.RequestClientCredentialsAsync("api1").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+               // return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);*/
+            #endregion
+
+
+            #region Resource Owner (userame/password)
+            var tokenClient = new TokenClient("http://localhost:5000/connect/token", "ro.client", "secret");
+            var tokenResponse = tokenClient.RequestResourceOwnerPasswordAsync("alice","password", "api1").Result;
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                // return;
+            }
+
+            Console.WriteLine(tokenResponse.Json); 
+
+
+
+
+
+            #endregion
+
+
+
+            #region Calling API
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response =  client.GetAsync("http://localhost:6000/api/values").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content =  response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Id4Server.QuickStartServer/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Id4Server.QuickStartServer
+{
+    public class Startup
+    {       
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                //.AddSigningCredential() -- For providing the production certificate public key
+                .AddInMemoryClients(Config.GetClients()) //List of Client Applications
+                .AddInMemoryApiResources(Config.GetApiResources()) //List of APIs
+                .AddTestUsers(Config.GetUsers()); //Need to replace with EF core
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+        }
+    }
+}
